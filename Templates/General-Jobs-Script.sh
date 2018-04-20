f63dc0d065eea0b91f193bfb93e66c9d01d7a878
rctraining <33338434+rctraining@users.noreply.github.com>
2018-04-20 13:52:31 -0600

    Create General-Jobs-Script.sh

---
+++ Templates/General-Jobs-Script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --nodes=1                    		# Number of requested nodes
+#SBATCH --time=0:05:00               		# Max wall time
+#SBATCH --qos=debug                  		# Specify debug QOS
+#SBATCH --partition=shas             		# Specify Summit haswell nodes
+#SBATCH --ntasks=24          	 	        # Number of tasks per job
+#SBATCH --job-name=Matlab_Gen_Parallel          # Job submission name
+#SBATCH --output=MATLAB_GEN_PARALLEL.%j.out     # Output file name with Job ID
+
+
+# Written by:	Shelley Knuth
+# Date:		24 February 2014
+# Updated:	21 April 2017
+# Purpose: 	This script calls a Matlab parallel program 
+
+# purge all existing modules
+module purge
+
+# load the matlab module
+module load matlab
+
+# The directory where you want the job to run
+cd /projects/$USER/tutorials/parallelization_workshop/new
+
+# Run matlab without a GUI and ask for all available workers
+matlab -nosplash -nodesktop -r "clear; num_workers=$SLURM_NTASKS; parallel_std;"
