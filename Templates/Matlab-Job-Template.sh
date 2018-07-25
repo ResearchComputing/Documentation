214271a30e011fe2ea5c376efb7fa5c7e56b6e51
rctraining <33338434+rctraining@users.noreply.github.com>
2018-07-25 12:09:43 -0600

    Update Matlab-Job-Template.sh
    Fixed an issue where the partition did not match the qos

--- Templates/Matlab-Job-Template.sh
+++ Templates/Matlab-Job-Template.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --nodes=1                    		# Number of requested nodes
-#SBATCH --time=0:05:00               		# Max wall time
-#SBATCH --qos=tesing                    # Specify testing QOS
-#SBATCH --partition=shas             		# Specify Summit haswell nodes
-#SBATCH --ntasks=24          	 	        # Number of tasks per job
+#SBATCH --nodes=1                    		        # Number of requested nodes
+#SBATCH --time=0:05:00               		        # Max wall time
+#SBATCH --qos=tesing                            # Specify testing QOS
+#SBATCH --partition=shas-testing             		# Specify Summit haswell nodes
+#SBATCH --ntasks=24          	 	                # Number of tasks per job
 #SBATCH --job-name=Matlab_Gen_Parallel          # Job submission name
 #SBATCH --output=MATLAB_GEN_PARALLEL.%j.out     # Output file name with Job ID
 
 
 # Written by:	Shelley Knuth
 # Date:		24 February 2014
-# Updated:	21 April 2017
+# Updated:	25 July 2017
 # Purpose: 	This script calls a Matlab parallel program 
 
 # purge all existing modules
