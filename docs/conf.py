c823899cb2951d8019b2f413a284dba6d03b83be
b-reyes <reyesb123@gmail.com>
2024-07-12 14:22:26 -0600

    clean-up conda_dev_env.yml and conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,8 @@
-# from recommonmark.parser import CommonMarkParser
-
 project = "Research Computing\nUniversity of Colorado Boulder"
 
 master_doc = 'index'
 
-# source_parsers = {
-#     '.md': CommonMarkParser,
-# }
-
-# extensions = [
-#     'sphinx_markdown_tables'
-# ]
-
 extensions = ['myst_parser']
-
-# source_suffix = ['.rst', '.md']
 
 source_suffix = {
     '.rst': 'restructuredtext',
