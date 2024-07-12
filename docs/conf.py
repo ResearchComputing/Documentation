1de5d537f1e3460ae8fe0a37372acd2a08534570
b-reyes <reyesb123@gmail.com>
2024-07-12 14:17:15 -0600

    change conda file and conf.py for new theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,19 +1,27 @@
-from recommonmark.parser import CommonMarkParser
+# from recommonmark.parser import CommonMarkParser
 
 project = "Research Computing\nUniversity of Colorado Boulder"
 
 master_doc = 'index'
 
-source_parsers = {
-    '.md': CommonMarkParser,
+# source_parsers = {
+#     '.md': CommonMarkParser,
+# }
+
+# extensions = [
+#     'sphinx_markdown_tables'
+# ]
+
+extensions = ['myst_parser']
+
+# source_suffix = ['.rst', '.md']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
 }
 
-extensions = [
-    'sphinx_markdown_tables'
-]
-
-source_suffix = ['.rst', '.md']
-
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 html_static_path = ['_static']
