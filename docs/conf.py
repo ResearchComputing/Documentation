e3c8ce95f84b243aafe663df58d16bff4a2fa235
b-reyes <reyesb123@gmail.com>
2024-07-15 22:14:59 -0600

    add light/dark logo, add custom css so that left sidebar is all the way to the left, add GitHub button with link to our docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,3 +15,13 @@ html_theme = 'sphinx_book_theme'
 myst_heading_anchors=6
 
 html_static_path = ['_static']
+html_css_files = ["custom.css"]
+
+html_theme_options = {
+   "logo": {
+      "image_light": "_static/Research_Computing_black_letters.png",
+      "image_dark": "_static/Research_Computing_white_letters.png",
+   }, 
+   "repository_url": "https://github.com/ResearchComputing/Documentation",
+   "use_repository_button": True,
+}
