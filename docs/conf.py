916736fb8373bd0f4f83e70277db87ad6793be4d
John Reiland <93950341+jreiland@users.noreply.github.com>
2025-08-29 18:10:36 -0600

    Fix acompile name

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ myst_substitutions = {
    'alpine_ucb_total_a100_gpu_nodes': '7',
    'alpine_ucb_total_mi100_gpu_nodes': '8',
    'alpine_ucb_total_gh200_gpu_nodes': '2',
-   'alpine_ucb_total_64_core_256GB_cpu_nodes_atesting': '4',
+   'alpine_ucb_total_64_core_256GB_cpu_nodes_acompile': '4',
    'alpine_ucb_total_atesting_a100_gpu_nodes': '1',
    'alpine_ucb_total_atesting_mi100_gpu_nodes': '8',
    ## AMC contributions
@@ -60,7 +60,7 @@ myst_substitutions = {
    'alpine_total_amem_nodes': '24',
    'alpine_total_csu_nodes': '77',
    'alpine_total_amc_nodes': '37',
-   'alpine_total_atesting_nodes': '4',
+   'alpine_total_acompile_nodes': '4',
    'alpine_total_atesting_a100_nodes': '1',
    'alpine_total_atesting_mi100_nodes': '8'
 }
