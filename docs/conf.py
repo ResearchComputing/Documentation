236d3954cba5f089fd9205db34cf331d870590e7
monaghaa <monaghaa@colorado.edu>
2026-08-13 14:49:29 -0600

    Alpine allocation updates

--- docs/conf.py
+++ docs/conf.py
@@ -11,11 +11,11 @@ myst_enable_checkboxes = True
 myst_substitutions = {
 
    # Allocation specific substitutions 
-   'trailhead_SUs': '2,000',
-   'boulder_ascent_SUs': '350,000', 
-   'boulder_ascent_SU_group_limit': '1.4 M', 
-   'boulder_peak_SUs': '6,000,000', 
-   'rmacc_ascent_SUs': '100,000',
+   'trailhead_SUs': 'NA',
+   'boulder_ascent_SUs': '450,000', 
+   'boulder_ascent_SU_group_limit': '1.8 M', 
+   'boulder_peak_SUs': '7,500,000', 
+   'rmacc_ascent_SUs': '250,000',
 
    # Alpine compute total counts substitutions 
    'alpine_total_compute_nodes': '489', 
